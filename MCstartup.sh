85e51869183d7b8e26377a34041e0a615b94595b
Chen Xue <71714024+Aristophanesss@users.noreply.github.com>
2023-06-11 15:48:49 -0700

    Add MCstartup.sh

---
+++ MCstartup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Update default packages
+sudo yum update -y
+
+# Install Java Development Kit
+sudo yum install -y java-17-amazon-corretto
+
+# Download Minecraft server JAR file
+curl -O https://piston-data.mojang.com/v1/objects/15c777e2cfe0556eef19aab534b186c0c6f277e1/server.jar
+
+# Start the Minecraft server for the first time
+java -Xmx1024M -Xms1024M -jar server.jar nogui
+
+# Agree to the Minecraft EULA
+sed -i 's/eula=false/eula=true/g' eula.txt
+
+# Create a systemd service file for Minecraft server
+sudo tee /etc/systemd/system/minecraft.service > /dev/null <<EOT
+[Unit]
+Description=Minecraft Server
+After=network.target
+
+[Service]
+User=ec2-user
+ExecStart=/usr/bin/java -Xmx1024M -Xms1024M -jar /home/ec2-user/server.jar nogui
+WorkingDirectory=/home/ec2-user
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+# Enable the Minecraft service
+sudo systemctl enable minecraft.service
+sudo systemctl start minecraft
--- main.tf
+++ main.tf
@@ -10,14 +10,63 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region = "us-west-2"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-080f7286ffdf988ee"
-  instance_type = "t2.medium"
+resource "aws_security_group" "minecraft_sg" {
+  name        = "MinecraftServerSG"
+  description = "Security group for Minecraft server"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 25565
+    to_port     = 25565
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "MinecraftServerSG"
+  }
+}
+
+resource "aws_instance" "minecraft_server" {
+  ami              = "ami-080f7286ffdf988ee"
+  instance_type    = "t2.medium"
+  vpc_security_group_ids = [aws_security_group.minecraft_sg.id]
+  key_name         = "minecraft"
 
   tags = {
     Name = "MinecraftServer"
   }
+
+  provisioner "file" {
+    content     = file("MCstartup.sh")
+    destination = "/home/ec2-user/MCstartup.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /home/ec2-user/MCstartup.sh",
+      "/home/ec2-user/MCstartup.sh"
+    ]
+  }
+}
+
+output "instance_public_ip" {
+  value       = aws_instance.minecraft_server.public_ip
+  description = "Public IP address of the instance."
 }
