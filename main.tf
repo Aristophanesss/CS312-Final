aca9018bf6c21ab6fdb6742dd9680da1211475dd
Chen Xue <71714024+Aristophanesss@users.noreply.github.com>
2023-06-11 14:35:15 -0700

    Add main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-080f7286ffdf988ee"
+  instance_type = "t2.medium"
+
+  tags = {
+    Name = "MinecraftServer"
+  }
+}
